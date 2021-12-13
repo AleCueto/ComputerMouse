181575b44b5c620380fb756851f097e0958155ef
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-12-13 14:25:02 +0100

    source code

---
+++ javascript/javascript.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+} else {
+    mybutton.style.display = "none";
+}
+}
+t
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} 
+
+function hamburguer() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
